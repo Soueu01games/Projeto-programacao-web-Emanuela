605d1b7cdd2ae6a4dacf928f4ed72407b25ece16
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:23:27 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//Serve para fazer a conexão entre o servidor do código e do banco de dados
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASS', '');
+	define('BASE', 'clinica');
+
+    $conn= new MySQLi(HOST, USER, PASS, BASE);
+//Apenas para garantir que conectoi
+	if($conn==true){
+		print "Conectou ao banco";
+	}
+	else{
+		print "Não foi possível conectar";
+	}
+?>
