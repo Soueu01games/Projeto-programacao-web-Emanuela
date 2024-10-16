83e853bd7cb17acf86f8650cd257590a978b67eb
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:23:59 -0300

    Create banco_de_dados.sql

---
+++ banco_de_dados.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Consulta (
+  id_consulta INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  Paciente_id_paciente INTEGER UNSIGNED NOT NULL,
+  Médico_id_medico INTEGER UNSIGNED NOT NULL,
+  dia_consulta DATE NULL,
+  hora_consulta TIME NULL,
+  descrição_consulta TEXT NULL,
+  PRIMARY KEY(id_consulta),
+  INDEX Consulta_FKIndex1(Médico_id_medico),
+  INDEX Consulta_FKIndex2(Paciente_id_paciente)
+);
+
+CREATE TABLE Médico (
+  id_medico INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome_medico VARCHAR(100) NULL,
+  crm_medico VARCHAR(10) NULL,
+  especialidade VARCHAR(20) NULL,
+  PRIMARY KEY(id_medico)
+);
+
+CREATE TABLE Paciente (
+  id_paciente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome_paciente VARCHAR(100) NULL,
+  cpf_paciente VARCHAR(14) NULL,
+  dt_nasc_paciente DATE NULL,
+  sexo_paciente  CHAR(1) NULL,
+  fone_paciente VARCHAR(20) NULL,
+  email_paciente VARCHAR(100) NULL,
+  endereco_paciente VARCHAR(100) NULL,
+  PRIMARY KEY(id_paciente)
+);
