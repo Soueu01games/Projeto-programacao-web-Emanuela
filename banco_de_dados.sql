bacbf982a339e3a5d8005fee2b9593d1c483bbd6
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:49:20 -0300

    Update banco_de_dados.sql

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -22,7 +22,7 @@ CREATE TABLE Paciente (
   id_paciente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   nome_paciente VARCHAR(100) NULL,
   cpf_paciente VARCHAR(14) NULL,
-  dt_nasc_paciente DATE NULL,
+  data_nasc_paciente DATE NULL,
   sexo_paciente  CHAR(1) NULL,
   fone_paciente VARCHAR(20) NULL,
   email_paciente VARCHAR(100) NULL,
