ad1f416e863d98cf075970738ba7f00728f91eff
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:21:09 -0300

    Update cadastrar-consulta.php

--- cadastrar-consulta.php
+++ cadastrar-consulta.php
@@ -1,1 +1,19 @@
 <h1>Cadastrar Consulta</h1>
+<form action="?page=salvar-consulta" method="POST">
+	<input type="hidden" name="acao" value="cadastrar">
+	<div class="mb-3">
+		<label>Dia</label>
+		<input type="date" name="dia_consulta" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Hora</label>
+		<input type="text" name="hora_consulta" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Descrição</label>
+		<input type="text" name="descricao_consulta" class="form-control">
+	</div>
+	<div class="mb-3">
+		<button type="submit" class="btn btn-sucess">Salvar</button>
+	</div>
+</form>
