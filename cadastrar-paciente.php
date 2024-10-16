cb0879a3f652755b299462d2c700a89267ea9d5e
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:22:04 -0300

    Update cadastrar-paciente.php

--- cadastrar-paciente.php
+++ cadastrar-paciente.php
@@ -1,1 +1,35 @@
 <h1>Cadastrar Paciente</h1>
+<form action="?page=salvar-paciente" method="POST">
+	<input type="hidden" name="acao" value="cadastrar">
+	<div class="mb-3">
+		<label>Nome</label>
+		<input type="text" name="nome_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>CPF</label>
+		<input type="text" name="cpf_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Data de Nascimento</label>
+		<input type="date" name="data_nasc_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Sexo</label>
+		<input type="text" name="sexo_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<labelT>Telefone</label>
+		<input type="text" name="fone_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Email</label>
+		<input type="text" name="email_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Endereço</label>
+		<input type="text" name="endereco_paciente" class="form-control">
+	</div>
+	<div class="mb-3">
+		<button type="submit" class="btn btn-sucess">Salvar</button>
+	</div>
+</form>
