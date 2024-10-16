0a63950bd3c446ac960fb031e48bb40adf79ab5e
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-16 14:22:28 -0300

    Update cadastrar-medico.php

--- cadastrar-medico.php
+++ cadastrar-medico.php
@@ -1,1 +1,19 @@
 <h1>Cadastrar Médico</h1>
+<form action="?page=salvar-medico" method="POST">
+	<input type="hidden" name="acao" value="cadastrar">
+	<div class="mb-3">
+		<label>Nome do Médico</label>
+		<input type="text" name="nome_medico" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>CRM</label>
+		<input type="text" name="crm_medico" class="form-control">
+	</div>
+	<div class="mb-3">
+		<label>Especialidade</label>
+		<input type="text" name="especialidade_medico" class="form-control">
+	</div>
+	<div class="mb-3">
+		<button type="submit" class="btn btn-sucess">Salvar</button>
+	</div>
+</form>
