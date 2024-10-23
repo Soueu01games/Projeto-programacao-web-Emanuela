e22ee0a58a1984d7b6eb9412b970fc473361cbb6
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-23 10:01:50 -0300

    Update listar-medico.php
    Cria lista com os médicos cadastrados

--- listar-medico.php
+++ listar-medico.php
@@ -1,1 +1,33 @@
 <h1>Listar Médico</h1>
+<?php
+include 'C:\xampp\htdocs\Emanuela\Projeto\config.php';
+$sql = "SELECT * FROM `médico`";
+
+$res = $conn->query($sql);
+
+$qtd = $res->num_rows;
+
+if($qtd > 0){
+	print"<p>Encontrou <b>$qtd</b> resultado(s).</p>";
+	print"<table class='table table-bordered table-striped table-hover'>";
+	print"<tr>";
+	print"<th>#</th>";
+	print"<th>Nome</th>";
+	print"<th>CRM</th>";
+	print"<th>Especialidade</th>";
+	print"</tr>";
+	while($row=$res->fetch_object()){
+		print"<tr>";
+		print "<td>".$row->id_medico."</td>";
+		print "<td>".$row->nome_medico."</td>";
+		print "<td>".$row->crm_medico."</td>";
+		print "<td>".$row->especialidade_medico."</td>";
+		print"</tr>";
+	}
+	print"</table>";
+	}
+else{
+	print"<p>Não foram encontrados <b>$qtd</b> resultado(s).</p>";
+	}	
+
+?>
