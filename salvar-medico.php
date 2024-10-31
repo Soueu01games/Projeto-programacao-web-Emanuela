14d1e1588cdda06ca45cb595e0d64d93ec619f46
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-31 17:09:34 -0300

    Update salvar-medico.php
    Adicionei update e exclude para o médico

--- salvar-medico.php
+++ salvar-medico.php
@@ -1,10 +1,10 @@
 <?php 
-include 'C:\xampp\htdocs\Emanuela\Projeto\config.php';//Diretório do arquivo config
+include 'E:\Xampp\htdocs\programaçaoweb\config.php';//Diretório do arquivo config
 switch (@$_REQUEST['acao']) {
 	case 'cadastrar':
-		$nome = $_REQUEST['nome_medico'];
-		$crm = $_REQUEST['crm_medico'];
-		$especialidade = $_REQUEST['especialidade_medico'];
+		$nome = $_POST['nome_medico'];
+		$crm = $_POST['crm_medico'];
+		$especialidade = $_POST['especialidade_medico'];
 		$sql = "INSERT INTO médico (
 			nome_medico, 
 			crm_medico, 
@@ -16,22 +16,51 @@ switch (@$_REQUEST['acao']) {
 		)";
 		$res = $conn->query($sql);	
 
-if($res==true){
+		if($res==true){
 		print"<script>alert('Cadastrou com sucesso');</script>";
 		print"<script>location.href='?page=listar-medico';</script>";
-	}	
-	else{
+		}	
+		else{
 		print"<script>alert('Deu errado');</script>";
 		print"<script>location.href='?page=listar-medico';</script>";
-	}
+		}
 
 		break;
 	case 'editar':
-		// código para editar
+		$nome = $_POST['nome_medico'];
+		$crm = $_POST['crm_medico'];
+		$especialidade = $_POST['especialidade_medico'];
+		$sql = "UPDATE médico SET
+					nome_medico='{$nome}', 
+					crm_medico='{$crm}', 
+					especialidade_medico='{$especialidade}'
+				WHERE
+					id_medico=".$_POST['id_medico'];
+
+		$res = $conn->query($sql);	
+		if($res==true){
+		print"<script>alert('Editou com sucesso');</script>";
+		print"<script>location.href='?page=listar-medico';</script>";
+		}	
+		else{
+		print"<script>alert('Deu errado');</script>";
+		print"<script>location.href='?page=listar-medico';</script>";
+		}
 		break;
-	case 'salvar':
-		// código para salvar
+	case 'excluir':
+		$sql = "DELETE FROM médico WHERE id_medico=".$_REQUEST['id_medico'];
+
+		$res = $conn->query($sql);	
+		if($res==true){
+		print"<script>alert('Excluiu com sucesso');</script>";
+		print"<script>location.href='?page=listar-medico';</script>";
+		}	
+		else{
+		print"<script>alert('Deu errado');</script>";
+		print"<script>location.href='?page=listar-medico';</script>";
+		}
 		break;
+
 	default:
 		// código padrão
 		break;
